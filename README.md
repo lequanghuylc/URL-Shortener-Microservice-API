f0811cdca0bc3d80908310beb1a644b0a04f6c81
quanghuyf <quanghuy@first-college.vn>
2016-08-17 09:28:59 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # URL-Shortener-Microservice-API
 Node application that shortens URL and return in JSON
+
+## Live demo
+[Click this c9 demo](https://ide.c9.io/quanghuyf/urlshorten) : hosted on c9. and it' ide link. You have to log into ild end run the application.
+
+## Live demo instruction
+1. Go to ide, (login using your account, it's free!)
+2. Run the command __./mongod__ in terminal
+3. Make sure file index.js has been Opened (appear in text editor)
+4. click __Run__ and visit the application's url (or run the command __node index.js__ in terminal, and click __Preview > Preview Running Application__, no need to do step 3 if you run application by this method)
---
+++ index.js
@@ -0,0 +1,77 @@
+var express = require("express");
+var app = express();
+var mongo = require("mongodb").MongoClient;
+var ObjectId = require('mongodb').ObjectId;
+var url = 'mongodb://localhost:27017/urlshorten';
+
+app.use('/', express.static(__dirname + '/public'));
+app.use(function(req,res){
+    // create data
+    if(req.url.indexOf("/new/") == 0){
+        var urlToVerify = req.url.substring(5);
+        var reg = /http(s)?:\/\/.?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g ;
+        
+        
+        // check if url is valid, if not, redirect to home page
+        if(!urlToVerify.match(reg)){
+            res.redirect("/");
+        } else {
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            //check if this url has been shorten before
+            mongo.connect(url, function(err, db){
+                if(err){throw err}
+                console.log("Connected correctly to server");
+                var newData = db.collection("url");
+                newData.find({
+                    "original_url": urlToVerify
+                }).toArray(function(err, docs){
+                    console.log(docs);
+                    if(err){throw err}
+                    if(docs.length > 0){ // this url has been shorten, load it from database
+                        res.end(JSON.stringify({
+                            "original_url": docs[0].original_url,
+                            "short_url": "https://urlshorten-quanghuyf.c9users.io/"+docs[0]._id
+                        }));
+                        db.close();
+                    } else { // this url hasn't been shorten yet, insert it from database
+                        newData.insert({
+                            "original_url": urlToVerify
+                        }, function(err, record){
+                            if(err){throw err}
+                            console.log("new url has been shorten");
+                            res.end(JSON.stringify({
+                                "original_url": urlToVerify,
+                                "short_url":"https://urlshorten-quanghuyf.c9users.io/"+record.ops[0]._id
+                            }));
+                            db.close();
+                        });
+                    }
+                });
+            });
+            
+        }
+    } else {
+        // check if _id in data
+        var idToFind = req.url.substring(1);
+        mongo.connect(url, function(err, db){
+           if(err){throw err}
+           console.log("Connected correctly to server, to find url");
+           var oldData = db.collection("url");
+           oldData.find({
+               "_id": ObjectId(idToFind)
+           }).toArray(function(err, docs){
+              if(err){throw err}
+              if(docs.length>0){
+                  res.redirect(docs[0].original_url);
+                  db.close();
+              } else {
+                  res.redirect("/");
+                  db.close();
+              }
+           });
+        });
+        
+    }
+});
+
+app.listen(8080);
